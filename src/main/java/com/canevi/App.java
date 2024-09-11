0d505d7c793ddf1c67044d18d734b2acdc77ed1b
YahyaBekirCanevi <93769780+YahyaBekirCanevi@users.noreply.github.com>
2024-09-11 13:20:15 +0300

    added java swing with maven

---
+++ src/main/java/com/canevi/App.java
@@ -0,0 +1,15 @@
+package com.canevi;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class App {
+    private JPanel panelMain;
+    private JButton buttonOk;
+
+    public App() {
+        buttonOk.addActionListener((ActionEvent e) -> {
+
+        });
+    }
+}
