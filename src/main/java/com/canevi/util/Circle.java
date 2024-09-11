6ee24ddeeed4ddda2d59714f42cd5cc6895ac9c5
mcanevi3 <m.canevi3@gmail.com>
2024-09-11 15:27:51 +0300

    circle

---
+++ src/main/java/com/canevi/util/Circle.java
@@ -0,0 +1,50 @@
+package com.canevi.util;
+
+import com.canevi.export.Exportable;
+
+public class Circle implements Exportable
+{
+    private Radius radius;
+    private Coordinate coordinate;
+
+    public Circle(){
+        this.coordinate=new Coordinate(1,0);
+        this.radius=new Radius(1);
+    }
+    public Circle(Coordinate c,Radius r){
+        this.coordinate=c;
+        this.radius=r;
+    }
+
+    public void SetRadius(Radius radius) {
+        this.radius = radius;
+    }
+
+    public void SetCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Radius GetRadius() {
+        return radius;
+    }
+
+    public Coordinate GetCoordinate() {
+        return coordinate;
+    }
+
+
+    @Override
+    public String export() {
+        String str="";
+        str+="\\draw (";
+        str+=this.coordinate.GetX()+","+this.coordinate.GetY()+")";
+        str+=" circle ("+this.radius.GetRadius()+");";
+        return str;
+    }
+
+    public static void main(String[] args)
+    {
+        Circle c=new Circle(new Coordinate(2,2),new Radius(3));
+        System.out.println(c.export());
+    }
+}
